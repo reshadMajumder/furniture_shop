bb8e22b351d9b45893676b5d533f552404bfafd8
Jahidul Hassan Reshad <reshad22205341197@diu.edu.bd>
2024-01-28 09:39:09 +0600

    ok

--- core/about/urls.py
+++
@@ -1,5 +0,0 @@
-from django.urls import path,include
-from .views import about
-urlpatterns = [
-    path('',about,name='about'),
-]
--- core/shop/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def shop(request):
-    return render(request, 'shop.html')
